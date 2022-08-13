8cdbf8563c56df24ea52b7d501045e4828ab0a9a
Mike Ci <dq042000@gmail.com>
2022-08-13 22:46:01 +0800

    版本配置

---
+++ web/vite/src/layout/Menu/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="menu">選單區域</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.menu {
+    width: 200px;
+    border-right: 1px solid #ccc;
+}
+</style>
