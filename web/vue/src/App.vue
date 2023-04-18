daaafe2462930bbc543148fa4fe2be9b07a387a4
祁威德 <dq042000@gmail.com>
2023-04-18 14:17:03 +0800

    調整語系

--- web/vue/src/App.vue
+++ web/vue/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <header>
+  <el-config-provider :locale="zhTw">
     <div class="wrapper">
       <router-view />
     </div>
-  </header>
+  </el-config-provider>
 </template>
 <script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import zhTw from 'element-plus/lib/locale/lang/zh-tw'
 </script>
