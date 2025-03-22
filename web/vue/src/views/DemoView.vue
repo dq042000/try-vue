3646e9e34fdd804908252ed1f7aa2f21d9c9c9a5
祁威德 <dq042000@gmail.com>
2025-03-22 23:22:54 +0800

    feat(): test code

---
+++ web/vue/src/views/DemoView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const fruits = ref<string[]>(['apple', 'banana', 'cherry'])
+
+function reorder() {
+  fruits.value = [fruits.value[2], fruits.value[0], fruits.value[1]]
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <div @click="reorder" class="button">重新排序</div>
+    <div v-for="name in fruits" :key="name" class="fruits">
+      <span>{{ name }}</span>
+      <input type="number">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
